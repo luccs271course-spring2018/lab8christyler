fff2fd2fb5802d5ceacfa04736267c1bd3d8c215
tnguyen28 <tnguyen28@luc.edu>
2017-11-07 22:20:04 -0600

    push

--- src/main/java/edu/luc/cs271/wordcount/Main.java
+++ src/main/java/edu/luc/cs271/wordcount/Main.java
@@ -9,14 +9,40 @@ public class Main {
     // set up the scanner so that it separates words based on space and punctuation
     final Scanner input = new Scanner(System.in).useDelimiter("[^\\p{Alnum}]+");
 
-    // TODO complete this main program
+    // TODO complete this main program.
+
+    Map<String, Integer> ui = new HashMap<String, Integer>();
+
+    WordCounter c = new WordCounter(ui);
+
+    c.countWords(input);
+
+    int size = c.getCounts().size();
+
+    List<Map.Entry<String, Integer>> l = new ArrayList<>(size);
+
+    l.addAll(c.getCounts().entrySet());
+
+    Collections.sort(l, new DescendingByCount());
+
+    if (c.getCounts().size() < 11) {
+      for (int i = 0; i < c.getCounts().size(); i++) {
+        System.out.println(l.get(i));
+      }
+    } else {
+      for (int i = 0; i < 10; i++) {
+        System.out.println(l.get(i));
+      }
+    }
+
     // 1. create a WordCounter instance
     // 2. use this to count the words in the input
     // 3. determine the size of the resulting map
     // 4. create an ArrayList of that size and
     // 5. store the map's entries in it (these are of type Map.Entry<String, Integer>
     // 6. sort the ArrayList in descending order by count
-    //    using Collections.sort and an instance of the provided comparator (after fixing the latter)
+    //    using Collections.sort and an instance of the provided comparator (after fixing the
+    // latter)
     // 7. print the (up to) ten most frequent words in the text
 
   }
--- src/test/java/edu/luc/cs271/wordcount/TestWordCounter.java
+++ src/test/java/edu/luc/cs271/wordcount/TestWordCounter.java
@@ -2,41 +2,47 @@ package edu.luc.cs271.wordcount;
 
 import static org.junit.Assert.*;
 
+import java.util.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestWordCounter {
 
-  // TODO complete this test class
+    // TODO complete this test class
+    // TODO declare a reference to the SUT (system under test), i.e., WordCounter
+    WordCounter wc;
 
-  // TODO declare a reference to the SUT (system under test), i.e., WordCounter
+    @Before
+    public void setUp() {
+        // TODO create the SUT instance
+        Map<String, Integer> m = new HashMap<String, Integer>();
+        wc = new WordCounter(m);
+    }
 
-  @Before
-  public void setUp() {
-    // TODO create the SUT instance
-  }
+    @After
+    public void tearDown() {
+        // TODO set the SUT instance to null
+        wc = null;
+    }
 
-  @After
-  public void tearDown() {
-    // TODO set the SUT instance to null
-  }
+    @Test
+    public void testGetCountEmpty() {
 
-  @Test
-  public void testGetCountEmpty() {
+        // TODO verify that the SUT initially returns an empty map.
+        assertEquals(wc.getCounts(), Collections.emptyMap());
+    }
 
-    // TODO verify that the SUT initially returns an empty map
-    fail();
+    @Test
+    public void testGetCountNonEmpty() {
 
-  }
-
-  @Test
-  public void testGetCountNonEmpty() {
-
-    // TODO run the SUT on a specific String iterator with some repeated words,
-    // then use assertions to verify the correct counts
-    // do this for at least two words in the iterator and two not in the iterator
-    fail();
-
-  }
+        // TODO run the SUT on a specific String iterator with some repeated words,
+        // then use assertions to verify the correct counts
+        wc.countWords(Arrays.asList("asdf", "oiu", "qwer").iterator());
+        // do this for at least two words in the iterator and two not in the iterator
+        assertEquals(wc.getCount("asdf"), 1);
+        assertEquals(wc.getCount("oiu"), 1);
+        assertEquals(wc.getCount("chris"), 0);
+        assertEquals(wc.getCount("tyler"), 0);
+    }
 }
